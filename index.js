207c257ef0da111258a78d3f4ad0baecfd7ab64e
Gustavo31519 <lsbgustavo@gmail.com>
2023-10-03 20:46:52 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json());
+
+app.listen(3333, () =>
+  console.log(`Running on http://localhost:${3333}`)
+);
+
+app.get("/", (req,res) => {
+    return res.json({message: "hello world"})
+})
